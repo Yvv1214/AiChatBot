554cea9eae6e57f600fa0789d7686db38bede97f
Yvv1214 <94neongreen@gmail.com>
2024-11-13 00:18:30 -0500

    correct tailwind config content

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./Frontend/index.html",
-    "./Frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
