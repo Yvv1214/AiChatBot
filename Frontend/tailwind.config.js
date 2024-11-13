1c93c5e7914a42918bc31b73fdedee1c70c79df5
Yvv1214 <94neongreen@gmail.com>
2024-11-12 21:11:28 -0500

    update content path to Frontend

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./Frontend/index.html",
+    "./Frontend/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
